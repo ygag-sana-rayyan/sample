c81e254f3b8702e3baace3562f0c8a84bbf92353
athulm <athularavind1999@gamil.com>
2024-07-19 10:05:04 +0530

    program editted

--- prgm1.py
+++ prgm1.py
@@ -3,26 +3,15 @@ from itertools import permutations
 
 s = "barfoothefoobarman"
 words = ["bar", "foo"]
-
 permutation = []
-
 for i in permutations(words):
-    print(i)
     permutation.append(''.join(i))
-
-print(permutation)
-
 s_permutation = []
-
 index_position = []
-
-for permu_s in permutation:
-    index = s.find(permu_s)
-    # if permu_s in s:
-    if permu_s:
-        s_permutation.append(permu_s)
+for perm in permutation:
+    index = s.find(perm)
+    if perm:
+        s_permutation.append(perm)
         index_position.append(index)
-
-print(s_permutation)
 print(index_position)
 
